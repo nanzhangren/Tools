0c0d948caace8c597e535a00764bd7791388f0ce
张腾飞 <trustfei@gmail.com>
2017-03-14 15:04:45 +0800

    Add convertKeyCodeToAsciiCode method.
    This method is used to convert a keyCode to ASCII code.

--- my method.js
+++ my method.js
@@ -1,5 +1,4 @@
-var const_object = 'object';
-
+// 1. isEqualObject
 // Compare whether the given two object are equal.
 // which means the reuslt will be true when the count of the two object is same and the relative field is equal.
 //
@@ -8,7 +7,8 @@ var const_object = 'object';
 //      {test: 123} & {test: 123}
 //      {test: 123, [1,2,3]} & {test: 123, [1,2,3]}
 //      {test: 123, {test: 234}} & {test: 123, {test: 234}}
-function isEqualObject(obj1, obj2, compareObjectFieldFunc) {
+var const_object = 'object';
+function isEqualObject(obj1, obj2, compareObjectFieldFunc) {    
     function isEqualArray(arr1, arr2) {
         if (!arr1 && !arr2) {
             return true;
@@ -70,4 +70,60 @@ function isEqualObject(obj1, obj2, compareObjectFieldFunc) {
         }
     }
     return true;
+}
+
+// 2. convertKeyCodeToAsciiCode
+// Convert keyCode in event to the ASCII code.
+function convertKeyCodeToAsciiCode(event) {
+	var keyCode = event.keyCode, shiftKey = event.shiftKey;
+	var keyMap = {
+		106: 42,    // * in number pad
+		107: 43,    // + in number pad
+		109: 45,    // - in number pad
+		110: 46,    // . in number pad
+		111: 47,    // / in number pad
+		173: 45     // - When in Firefore 15 press minus key, the keyCode is 173
+	}, generalKeyMap = {
+		186: 59,    // ;
+		187: 61,    // =
+		188: 44,    // ,
+		189: 45,    // -
+		190: 46,    // .
+		191: 47,    // /
+		192: 96,    // `
+		219: 91,    // [
+		220: 92,    // \
+		221: 93,    // ]
+		222: 39     // '
+	}, shiftKeyMap = {
+		186: 58,    // :
+		187: 43,    // +
+		188: 60,    // <
+		189: 95,    // _
+		190: 62,    // >
+		191: 63,    // ?
+		192: 126,   // ~
+		219: 123,   // {
+		220: 124,   // |
+		221: 125,   // }
+		222: 34     // "
+	};
+	var retCode = keyCode;
+	if(keyCode >= 96 && keyCode <= 105) {    // 0 ~ 9 in number pad
+		retCode = keyCode - 48;
+	} else if(keyMap[keyCode]) {
+		retCode = keyMap[keyCode];
+	} else if (shiftKey && shiftKeyMap[keyCode]) {
+		retCode = shiftKeyMap[keyCode];
+	} else if(!shiftKey && generalKeyMap[keyCode]) {
+		retCode = generalKeyMap[keyCode];
+	} else if(!shiftKey && keyCode >= 65 && keyCode <= 90) {    // eslint-disable-line      // a~z
+		retCode = keyCode + 32;
+	}
+	// KeyCode is same with ASCII letter:
+	//    A ~ Z  =>  65 ~ 90
+	//    0 ~ 9  =>  48 ~ 57 (number in letter pad)
+	//    space  =>  32
+	//    =      =>  61 // When in Firefore 15 and Opera, press equal key, the keyCode is 61
+	return retCode;
 }
